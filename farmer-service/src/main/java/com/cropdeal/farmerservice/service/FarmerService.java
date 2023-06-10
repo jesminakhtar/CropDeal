5df730db2ef8d2df9295a740a8079d22b82fdb4f
Jesmin Akhtar <akhtarjesmin2001@gmail.com>
2023-06-10 13:35:43 +0530

    Updated farmerService so that changes in inventory are reflaced in farmer crops

--- farmer-service/src/main/java/com/cropdeal/farmerservice/service/FarmerService.java
+++ farmer-service/src/main/java/com/cropdeal/farmerservice/service/FarmerService.java
@@ -8,12 +8,14 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import com.cropdeal.farmerservice.exception.InvalidFarmerException;
 import com.cropdeal.farmerservice.model.Crop;
 import com.cropdeal.farmerservice.model.Farmer;
-import com.cropdeal.farmerservice.repository.CropRepository;
+//import com.cropdeal.farmerservice.repository.CropRepository;
 import com.cropdeal.farmerservice.repository.FarmerRepository;
 
 @Service
@@ -22,21 +24,28 @@ public class FarmerService {
 	@Autowired
 	private FarmerRepository repository;
 
-	@Autowired
-	private CropRepository cropRepository;
+//	@Autowired
+//	private CropRepository cropRepository;
 
 	@Autowired
 	private RestTemplate restTemplate;
-	
+
 //	private static final String INVENTORY_SERVICE_URL = "http://inventory-service";
 	private static final String INVENTORY_SERVICE_URL = "http://localhost:8082";
 
 	public List<Farmer> getAllFarmers() {
-		return repository.findAll();
+		List<Farmer> farmers = repository.findAll();
+		for (Farmer farmer : farmers) {
+			updateProductDetails(farmer);
+		}
+		return farmers;
 	}
 
 	public Farmer getFarmerById(String id) throws InvalidFarmerException {
-		return repository.findById(id).orElseThrow(() -> new InvalidFarmerException("No farmer found with id " + id));
+		Farmer farmer = repository.findById(id)
+				.orElseThrow(() -> new InvalidFarmerException("No farmer found with id " + id));
+		updateProductDetails(farmer);
+		return farmer;
 	}
 
 	public Farmer addFarmer(Farmer farmer) {
@@ -56,76 +65,86 @@ public class FarmerService {
 	public ResponseEntity<String> addCrop(String farmerId, Crop crop) throws InvalidFarmerException {
 		Farmer farmer = getFarmerById(farmerId);
 		crop.setFarmerId(farmerId);
-		cropRepository.save(crop);
+//		cropRepository.save(crop);
 		farmer.addCrop(crop);
 		repository.save(farmer);
-		
 
 		// Make a POST request to the Inventory Service to add the crop
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Content-Type", "application/json");
-        HttpEntity<Crop> request = new HttpEntity<>(crop, headers);
-        ResponseEntity<String> response = restTemplate.postForEntity(
-                INVENTORY_SERVICE_URL + "/crops",
-                request,
-                String.class
-        );
-        
-        return response;
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Content-Type", "application/json");
+		HttpEntity<Crop> request = new HttpEntity<>(crop, headers);
+		ResponseEntity<String> response = restTemplate.postForEntity(INVENTORY_SERVICE_URL + "/crops", request,
+				String.class);
+
+		return response;
 	}
 
-	
 	public void updateCrop(String farmerId, String cropId, Crop updatedCrop) throws InvalidFarmerException {
-        Farmer farmer = getFarmerById(farmerId);
-        Crop existingCrop = getFarmerCropById(farmerId, cropId);
-        // Set the farmerId and ID of the updated crop to maintain the link with the farmer and the existing crop
-        updatedCrop.setFarmerId(farmer.getId());
-        updatedCrop.setId(existingCrop.getId());
+		Farmer farmer = getFarmerById(farmerId);
+		Crop existingCrop = getFarmerCropById(farmerId, cropId);
+		// Set the farmerId and ID of the updated crop to maintain the link with the
+		// farmer and the existing crop
+		updatedCrop.setFarmerId(farmer.getId());
+		updatedCrop.setId(existingCrop.getId());
 
-        // Make a PUT request to the Inventory Service to update the crop
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Content-Type", "application/json");
-        HttpEntity<Crop> request = new HttpEntity<>(updatedCrop, headers);
-        ResponseEntity<String> response = restTemplate.exchange(
-                INVENTORY_SERVICE_URL + "/crops/" + cropId,
-                HttpMethod.PUT,
-                request,
-                String.class
-        );
+		// Make a PUT request to the Inventory Service to update the crop
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Content-Type", "application/json");
+		HttpEntity<Crop> request = new HttpEntity<>(updatedCrop, headers);
+		ResponseEntity<String> response = restTemplate.exchange(INVENTORY_SERVICE_URL + "/crops/" + cropId,
+				HttpMethod.PUT, request, String.class);
 
-        if (!response.getStatusCode().is2xxSuccessful()) {
-            throw new InvalidFarmerException("Failed to update the crop in the inventory");
-        }
-    }
+		if (!response.getStatusCode().is2xxSuccessful()) {
+			throw new InvalidFarmerException("Failed to update the crop in the inventory");
+		}
+	}
 
-    public void deleteCrop(String farmerId, String cropId) throws InvalidFarmerException {
-        Farmer farmer = getFarmerById(farmerId);
-        Crop crop = getFarmerCropById(farmerId, cropId);
+	public void deleteCrop(String farmerId, String cropId) throws InvalidFarmerException {
+		Farmer farmer = getFarmerById(farmerId);
+		Crop crop = getFarmerCropById(farmerId, cropId);
 
-        farmer.removeCrop(crop);
-        
-        // Make a DELETE request to the Inventory Service to delete the crop
-        restTemplate.delete(INVENTORY_SERVICE_URL + "/crops/" + cropId);
-    }
+		farmer.removeCrop(crop);
 
-    private Crop getFarmerCropById(String farmerId, String cropId) throws InvalidFarmerException {
-        // Make a GET request to the Inventory Service to retrieve the crop by ID
-        ResponseEntity<Crop> response = restTemplate.getForEntity(
-                INVENTORY_SERVICE_URL + "/crops/" + cropId,
-                Crop.class
-        );
+		// Make a DELETE request to the Inventory Service to delete the crop
+		restTemplate.delete(INVENTORY_SERVICE_URL + "/crops/" + cropId);
+	}
 
-        if (response.getStatusCode().is2xxSuccessful()) {
-            Crop crop = response.getBody();
-            // Check if the retrieved crop belongs to the specified farmer
-            if (crop != null && crop.getFarmerId().equals(farmerId)) {
-                return crop;
-            } else {
-                throw new InvalidFarmerException("Crop not found for the specified farmer");
-            }
-        } else {
-            throw new InvalidFarmerException("Failed to retrieve the crop from the inventory");
-        }
-    }
+	private Crop getFarmerCropById(String farmerId, String cropId) throws InvalidFarmerException {
+		// Make a GET request to the Inventory Service to retrieve the crop by ID
+		ResponseEntity<Crop> response = restTemplate.getForEntity(INVENTORY_SERVICE_URL + "/crops/" + cropId,
+				Crop.class);
+
+		if (response.getStatusCode().is2xxSuccessful()) {
+			Crop crop = response.getBody();
+			// Check if the retrieved crop belongs to the specified farmer
+			if (crop != null && crop.getFarmerId().equals(farmerId)) {
+				return crop;
+			} else {
+				throw new InvalidFarmerException("Crop not found for the specified farmer");
+			}
+		} else {
+			throw new InvalidFarmerException("Failed to retrieve the crop from the inventory");
+		}
+	}
+
+	private void updateProductDetails(Farmer farmer) {
+		for (Crop crop : farmer.getCrops()) {
+			try {
+				// Retrieve product details from the inventory service
+				String productUrl = INVENTORY_SERVICE_URL + "/crops/" + crop.getId();
+				Crop inventoryCrop = restTemplate.getForObject(productUrl, Crop.class);
+
+				// Update the farmer's product details
+				crop.setName(inventoryCrop.getName());
+				crop.setPrice(inventoryCrop.getPrice());
+				crop.setQuantity(inventoryCrop.getQuantity());
+			} catch (HttpClientErrorException.NotFound | HttpServerErrorException.InternalServerError ex) {
+				// Handle the case when the crop is not found in the inventory service
+				
+				crop.setName("Unavailable");
+				crop.setQuantity(0);
+				crop.setPrice(0.0);
+			}
+		}
+	}
 }
-	
